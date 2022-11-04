d01e78d81050118f5cfa9f0f18db74d536f7adee
cpharris1 <cpharris1@crimson.ua.edu>
2022-11-04 12:23:31 -0500

    try fixing build

---
+++ firmware/pic32_picosecond_delay_control.X/build/default/production/_ext/1360937237/uart_helper.o.d
@@ -0,0 +1,51 @@
+build/default/production/_ext/1360937237/uart_helper.o: \
+ ../src/uart_helper.c ../src/config/default/definitions.h \
+ ../src/config/default/peripheral/coretimer/plib_coretimer.h \
+ ../src/config/default/peripheral/uart/plib_uart3.h \
+ ../src/config/default/device.h \
+ ../src/config/default/toolchain_specifics.h \
+ ../src/config/default/peripheral/uart/plib_uart_common.h \
+ ../src/config/default/peripheral/i2c/master/plib_i2c3_master.h \
+ ../src/config/default/peripheral/i2c/master/plib_i2c_master_common.h \
+ ../src/config/default/peripheral/cdac/plib_cdac1.h \
+ ../src/config/default/peripheral/clk/plib_clk.h \
+ ../src/config/default/peripheral/gpio/plib_gpio.h \
+ ../src/config/default/peripheral/evic/plib_evic.h \
+ ../src/config/default/peripheral/tmr1/plib_tmr1.h \
+ ../src/config/default/peripheral/tmr1/plib_tmr1_common.h \
+ ../src/config/default/peripheral/adc/plib_adc.h \
+ ../src/config/default/bsp/bsp.h ../src/uart_helper.h
+
+../src/config/default/definitions.h:
+
+../src/config/default/peripheral/coretimer/plib_coretimer.h:
+
+../src/config/default/peripheral/uart/plib_uart3.h:
+
+../src/config/default/device.h:
+
+../src/config/default/toolchain_specifics.h:
+
+../src/config/default/peripheral/uart/plib_uart_common.h:
+
+../src/config/default/peripheral/i2c/master/plib_i2c3_master.h:
+
+../src/config/default/peripheral/i2c/master/plib_i2c_master_common.h:
+
+../src/config/default/peripheral/cdac/plib_cdac1.h:
+
+../src/config/default/peripheral/clk/plib_clk.h:
+
+../src/config/default/peripheral/gpio/plib_gpio.h:
+
+../src/config/default/peripheral/evic/plib_evic.h:
+
+../src/config/default/peripheral/tmr1/plib_tmr1.h:
+
+../src/config/default/peripheral/tmr1/plib_tmr1_common.h:
+
+../src/config/default/peripheral/adc/plib_adc.h:
+
+../src/config/default/bsp/bsp.h:
+
+../src/uart_helper.h:
