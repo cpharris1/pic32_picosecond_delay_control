a85230ea8fa3a005d1906509fc40413a3978a241
Chloe Harris <44858411+cpharris1@users.noreply.github.com>
2022-11-04 12:29:30 -0500

    Fix build in mplab (#3)

---
+++ firmware/pic32_picosecond_delay_control.X/build/default/production/_ext/513455433/plib_i2c3_master.o.d
@@ -0,0 +1,14 @@
+build/default/production/_ext/513455433/plib_i2c3_master.o: \
+ ../src/config/default/peripheral/i2c/master/plib_i2c3_master.c \
+ ../src/config/default/device.h \
+ ../src/config/default/toolchain_specifics.h \
+ ../src/config/default/peripheral/i2c/master/plib_i2c3_master.h \
+ ../src/config/default/peripheral/i2c/master/plib_i2c_master_common.h
+
+../src/config/default/device.h:
+
+../src/config/default/toolchain_specifics.h:
+
+../src/config/default/peripheral/i2c/master/plib_i2c3_master.h:
+
+../src/config/default/peripheral/i2c/master/plib_i2c_master_common.h:
---
+++ firmware/pic32_picosecond_delay_control.X/build/default/production/_ext/60163342/plib_adc.o.d
@@ -0,0 +1,11 @@
+build/default/production/_ext/60163342/plib_adc.o: \
+ ../src/config/default/peripheral/adc/plib_adc.c \
+ ../src/config/default/device.h \
+ ../src/config/default/toolchain_specifics.h \
+ ../src/config/default/peripheral/adc/plib_adc.h
+
+../src/config/default/device.h:
+
+../src/config/default/toolchain_specifics.h:
+
+../src/config/default/peripheral/adc/plib_adc.h:
