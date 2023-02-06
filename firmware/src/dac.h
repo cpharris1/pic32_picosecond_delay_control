00fd9806c14b045095004ab9c39f3b5c030961f8
Chloe Harris <44858411+cpharris1@users.noreply.github.com>
2023-02-06 17:00:30 -0600

    refactor to use new DAC (#7)

--- firmware/src/dac.h
+++ firmware/src/dac.h
@@ -8,8 +8,8 @@
 #ifndef DAC_H
 #define	DAC_H
 
-#define MCP4725_ADDR                        0x62 //7 bit addr, A0 to GND
-#define MCP4725_WRITE_DAC                   0x40 // Write DAC cmd 010
+#define MCP4725_ADDR                        0x60 //7 bit addr, A0, A1 to GND
+#define MCP4725_WRITE_DAC                   0x00 // Write DAC cmd 00 to addr 0x00
 #define MCP4725_TX_DATA_LENGTH              3    // addr, cmd, high, low
 #define APP_ACK_DATA_LENGTH                 1
 
